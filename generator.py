ab0e7e8c13fd2f5c8256c063fefdc4af60532dbb
Riya Jain <jriya.shi@gmail.com>
2023-06-11 14:10:14 +0000

    Created generic Game and Generator class
    - Game class can be used to create a game with
      any configuration
    
    - This can be used to replace the hardcoded games
      like 2panewindow, 3panewindow etc
    
    - The class constructor takes in the background_image
      the key map for the controller and optional speed
      and optional border_width
    
    - Generator class is used in Game class to generate
      the spikes in game as defined by spikes_config
      defined for each game
    
    - It also handles the logic to delete the spikes
      when they have covered their allocated path

---
+++ generator.py
@@ -0,0 +1,35 @@
+from spike import Spike
+from random import randint
+
+
+class Generator:
+
+    def __init__(self):
+        self.spikes_config = []
+        self.spikes = []
+
+    def configure(self, config):
+        self.spikes = []
+        self.spikes_config = config
+        for _ in range(len(self.spikes_config)):
+            self.spikes.append([])
+
+    def generate(self, speed=7):
+        for i, config in enumerate(self.spikes_config):
+            if (len(self.spikes[i]) < config[3]):
+                side = int(randint(0, 1))
+                self.spikes[i].append(Spike(config[side][0],
+                                            config[side][1],
+                                            side, speed, config[2]))
+
+    def update(self, guys=[]):
+        for i, section_spikes in enumerate(self.spikes):
+            for j, spike in enumerate(section_spikes):
+                for guy in guys:
+                    if guy.check_collision(spike.x, spike.y,
+                                           spike.rect.width,
+                                           spike.rect.height):
+                        return True
+                if spike.update():
+                    del self.spikes[i][j]
+        return False
