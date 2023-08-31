a135fc829cda593d5742b4a812db3e7789cb4c2f
Riya Jain <riya1jain567@gmail.com>
2023-08-31 20:01:49 +0000

    Fix issue #31: Canvas overlaps outside screen - Before the back button was not   consistent over different screen   sizes, now it is visible and consitent   on different screen sizes
    - The help screen was also overflowing
      outside the canvas in some screen sizes
      which is also fixed now

--- howtoplay.py
+++ howtoplay.py
@@ -1,27 +1,45 @@
-
-from random import *
 import pygame
-import pickle
+from gi.repository import Gdk
 from gi.repository import Gtk
+from button import Button
+
+
+def color_parse(color):
+    rgba = Gdk.RGBA()
+    rgba.parse(color)
+    return (int(rgba.red * 255), int(rgba.green * 255), int(rgba.blue * 255))
 
 
 class rules:
 
     def run(self, gameDisplay, bg_dimensions, offset):
 
-        disp_width = 600
+        def back_action():
+            self.press = 1
 
-        press = 0
+        self.press = 0
+        screen_rect = gameDisplay.get_rect()
+        width = bg_dimensions[0]
+        height = screen_rect.height
         black = (0, 0, 0)
+        bg_color = color_parse("#f8eddd")  # Derived from rulesbackground.png
+
         clock = pygame.time.Clock()
         timer = pygame.time.Clock()
 
         background = pygame.image.load("data/images/rulesbackground.png")
         back = pygame.image.load("data/images/back.png")
 
-        background = pygame.transform.scale(
-            background, bg_dimensions)
+        bg_rect = background.get_rect()
+
+        # To maintain the aspect ratio of the image we calculate width according to screen height
+        bg_w = int(height * (bg_rect.width / bg_rect.height))
+        background = pygame.transform.scale(background, (bg_w, height))
+        bg_rect = background.get_rect()
+
         back = pygame.transform.scale(back, (70, 40))
+        back_btn = Button(offset[0] + width - 45, 25, "data/images/back.png",
+                          back_action, scale=[70, 40])
 
         sound = True
 
@@ -35,25 +53,24 @@ class rules:
             if event.type == pygame.QUIT:
                 return
 
+            if (pygame.mouse.get_pressed())[0] == 1 and self.press == 0:
+                self.press = 1
+                return
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.press = 0
+
             mos_x, mos_y = pygame.mouse.get_pos()
 
             gameDisplay.fill(black)
-            gameDisplay.blit(background, offset)
+            # Draw background
+            pygame.draw.rect(gameDisplay, bg_color, pygame.Rect(offset[0],
+                             offset[1], bg_dimensions[0], screen_rect.height))
 
-            if back.get_rect(center=(750 + 35, 10 + 15)).collidepoint(mos_x, mos_y):
-                gameDisplay.blit(pygame.transform.scale(
-                    back, (75, 45)), (725, 5))
-                if (pygame.mouse.get_pressed())[0] == 1 and press == 0:
-                    press = 1
-                    return
+            gameDisplay.blit(background, (offset[0] + bg_dimensions[0] / 2 - bg_rect.width / 2,
+                                          screen_rect.height - bg_rect.height))
 
-                if event.type == pygame.MOUSEBUTTONUP:
-                    press = 0
-
-            else:
-                gameDisplay.blit(back, (725, 5))
-
-            # pygame.draw.circle(gameDisplay,(255,255,255), (750+35,20+15),5,2)
+            back_btn.draw()
 
             pygame.display.update()
             clock.tick(60)
